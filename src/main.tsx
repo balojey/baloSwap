da2005e8e793e91ddfc27a97cc38291e699e7bae
ademolabalogun91 <ademolabalogun91@gmail.com>
2024-05-30 05:06:59 +0000

    Quick fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,13 @@ import App from './App.tsx'
 import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
-import { PetraWallet, PetraWalletName } from 'petra-plugin-wallet-adapter';
 
 // const wallets = [new PetraWallet()]
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%" appearance="dark">
-      <AptosWalletAdapterProvider optInWallets={[PetraWalletName, "Nightly", "T wallet"]} autoConnect={true}>
+      <AptosWalletAdapterProvider optInWallets={["Petra", "Nightly", "T wallet"]} autoConnect={true}>
         <App />
        </AptosWalletAdapterProvider> 
     </Theme>
